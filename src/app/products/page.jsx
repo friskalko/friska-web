05cd36c38e9b43525cd0f3d509bf94a72d1b4703
Rajat Verma <rajat311201@gmail.com>
2023-07-17 14:01:09 +0530

    product form complete

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,9 +1,52 @@
 "use client";
 import ProductForm from "@/components/ProductForm";
-import React, { useState } from "react";
-
+import Spinner from "@/components/Spinner";
+import { getAllCategories, getAllProducts } from "@/firebase/helpers";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { GoKebabHorizontal } from "react-icons/go";
 function Products() {
     const [formOpen, setFormOpen] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const [categories, setCategories] = useState([]);
+    const [categoriesMap, setCategoriesMap] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const respCat = await getAllCategories();
+
+                const tempCategoriesMap = {};
+                respCat.forEach((cat) => {
+                    tempCategoriesMap[cat.id] = cat.name;
+                });
+                setCategoriesMap(tempCategoriesMap);
+
+                setCategories(respCat);
+            } catch (error) {
+                console.log(error);
+            } finally {
+            }
+        })();
+    }, []);
+
+    const fetchProducts = async () => {
+        setLoading(true);
+        try {
+            const resp = await getAllProducts();
+            setProducts(resp);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
 
     return (
         <>
@@ -14,9 +57,83 @@ function Products() {
                 {formOpen ? "Close form" : "Add a new product"}
             </button>
 
-            {formOpen && <ProductForm onClose={setFormOpen} />}
+            {formOpen && (
+                <ProductForm onClose={setFormOpen} categories={categories} />
+            )}
+
+            {loading ? (
+                <Spinner />
+            ) : (
+                <div className="flex flex-col items-stretch gap-5 my-5">
+                    {products.map((product) => (
+                        <ProductCard
+                            key={product.id}
+                            product={product}
+                            categoriesMap={categoriesMap}
+                            categories={categories}
+                        />
+                    ))}
+                </div>
+            )}
+
+            {!loading && products.length === 0 && (
+                <div className="flex flex-col items-center justify-center gap-5 my-5">
+                    <p className="text-2xl font-semibold text-gray-800">
+                        No products found
+                    </p>
+                    <p className="text-gray-500">
+                        Add a new product to get started
+                    </p>
+                </div>
+            )}
         </>
     );
 }
 
 export default Products;
+
+const ProductCard = ({ product, categoriesMap, categories }) => {
+    const [editFormOpen, setEditFormOpen] = useState(false);
+
+    return (
+        <>
+            <div className="flex  items-stretch justify-between bg-white rounded-md shadow-md ">
+                {product.image && (
+                    <Image
+                        width={100}
+                        height={100}
+                        className="object-cover rounded-l-md h-20 w-auto "
+                        src={product.image}
+                        alt="Product Image"
+                        loading="lazy"
+                    />
+                )}
+
+                <div className="w-full p-4 py-2">
+                    <p className="text-lg font-medium text-gray-800 truncate">
+                        {product.name}
+                    </p>
+                    <p className="text-gray-500 font-light text-md">
+                        {categoriesMap[product.category]}
+                    </p>
+                </div>
+                <div
+                    className="flex items-center bg-blue-600 rounded-r-md cursor-pointer"
+                    onClick={() => {
+                        setEditFormOpen(!editFormOpen);
+                    }}
+                >
+                    <GoKebabHorizontal className="text-2xl  text-white rotate-90" />
+                </div>
+            </div>
+            {editFormOpen && (
+                <ProductForm
+                    onClose={setEditFormOpen}
+                    categories={categories}
+                    edit
+                    product={product}
+                />
+            )}
+        </>
+    );
+};
--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,28 +1,21 @@
 "use client";
 
-import { getAllCategories, createProduct } from "@/firebase/helpers";
+import {
+    getAllCategories,
+    createProduct,
+    updateProduct,
+} from "@/firebase/helpers";
+import { convertToCapitalWordsString } from "@/utils";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-export default function ProductForm({ onClose }) {
-    const [categories, setCategories] = useState([]);
-    const [imageFile, setImageFile] = useState(null);
+export default function ProductForm({ onClose, categories, edit, product }) {
+    const [imageFile, setImageFile] = useState(product?.image || null);
+    const [submitting, setSubmitting] = useState(false);
 
-    useEffect(() => {
-        try {
-            (async () => {
-                const respCat = await getAllCategories();
-                console.log(respCat);
-                setCategories(respCat);
-            })();
-        } catch (error) {
-            console.log(error);
-        } finally {
-        }
-    }, []);
-
-    function submitFormHandler(e) {
+    async function submitFormHandler(e) {
         e.preventDefault();
+        setSubmitting(true);
 
         const formData = new FormData(e.target);
 
@@ -34,31 +27,51 @@ export default function ProductForm({ onClose }) {
             image: imageFile,
         };
 
+        const specifications = [];
+        for (let i = 0; i < formData.getAll("key").length; i++) {
+            if (formData.getAll("key")[i].trim() === "") {
+                continue;
+            }
+            specifications.push({
+                key: convertToCapitalWordsString(formData.getAll("key")[i]),
+                value: convertToCapitalWordsString(formData.getAll("value")[i]),
+            });
+        }
+        productData.specifications = specifications;
+
         try {
-            (async () => {
-                const resp = await createProduct(productData);
-                console.log(resp);
-            })();
+            const resp = await (edit
+                ? updateProduct(product.id, productData, product)
+                : createProduct(productData));
+            console.log(resp);
+            window.location.reload();
         } catch (error) {
             console.log(error);
         }
+        setSubmitting(false);
     }
     return (
         <>
             <form
-                className="max-w-2xl my-5 mx-auto border border-gray-200 rounded-lg px-5 py-7 bg-white"
+                className="max-w-3xl min-w-[38rem] my-5 mx-auto border border-gray-200 rounded-lg px-5 py-7 bg-white"
                 onSubmit={submitFormHandler}
             >
                 <h1 className="text-3xl font-semibold text-center mb-4 text-gray-600">
-                    Add new product
+                    {edit ? "Edit product" : "Add new product"}
                 </h1>
                 <div className="form__grp">
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" required />
+                    <input
+                        type="text"
+                        name="name"
+                        required
+                        defaultValue={edit ? product.name : ""}
+                    />
                 </div>
                 <div className="form__grp">
                     <label htmlFor="summary">Summary</label>
                     <textarea
+                        defaultValue={edit ? product.summary : ""}
                         name="summary"
                         id="summary"
                         cols="25"
@@ -72,11 +85,16 @@ export default function ProductForm({ onClose }) {
                         id="description"
                         cols="25"
                         rows="3"
+                        defaultValue={edit ? product.description : ""}
                     ></textarea>
                 </div>
                 <div className="form__grp">
                     <label htmlFor="category">Category</label>
-                    <select name="category" id="category">
+                    <select
+                        name="category"
+                        id="category"
+                        defaultValue={edit ? product.category : ""}
+                    >
                         {categories.map((category) => (
                             <option key={category.id} value={category.id}>
                                 {category.name}
@@ -84,7 +102,7 @@ export default function ProductForm({ onClose }) {
                         ))}
                     </select>
                 </div>
-                <SpecificationsInput />
+                <SpecificationsInput edit={edit} product={product} />
 
                 {/* IMAGE  UPLOAD INPUT  */}
                 <div className="form__grp">
@@ -92,7 +110,8 @@ export default function ProductForm({ onClose }) {
                     <input
                         type="file"
                         name="image"
-                        required
+                        required={edit ? product.image !== imageFile : true}
+                        accept="image/*"
                         onChange={(e) => setImageFile(e.target.files[0])}
                     />
                     {imageFile && (
@@ -103,7 +122,13 @@ export default function ProductForm({ onClose }) {
                             <Image
                                 height={200}
                                 width={200}
-                                src={URL.createObjectURL(imageFile)}
+                                src={
+                                    !edit
+                                        ? URL.createObjectURL(imageFile)
+                                        : imageFile == product?.image
+                                        ? product?.image
+                                        : URL.createObjectURL(imageFile)
+                                }
                                 alt="Product Image"
                                 className="w-auto max-h-40 object-cover"
                             />
@@ -122,10 +147,17 @@ export default function ProductForm({ onClose }) {
                             Close Form
                         </button>
                         <button
+                            disabled={submitting}
                             type="submit"
-                            className="bg-blue-600 text-white px-5 py-2 rounded hover:bg-blue-500 font-semibold"
+                            className="bg-blue-600 text-white px-5 py-2 rounded hover:bg-blue-500 font-semibold  disabled:bg-gray-500 disabled:cursor-not-allowed"
                         >
-                            Add Product
+                            {edit
+                                ? submitting
+                                    ? "Saving..."
+                                    : "Save Changes"
+                                : submitting
+                                ? "Adding..."
+                                : "Add Product"}
                         </button>
                     </div>
                 </div>
@@ -133,31 +165,75 @@ export default function ProductForm({ onClose }) {
         </>
     );
 }
+const SpecificationsInput = ({ edit, product }) => {
+    const [specifications, setSpecifications] = useState(
+        edit ? product?.specifications : [{ key: "", value: "" }]
+    );
+    const [propertyCnt, setPropertyCnt] = useState(specifications?.length || 1);
 
-const SpecificationsInput = () => {
-    const [propertyCnt, setPropertyCnt] = useState(1);
+    const handleDelete = (index) => {
+        setSpecifications((prevSpecs) => {
+            const newSpecs = [...prevSpecs];
+            newSpecs.splice(index, 1);
+            return newSpecs;
+        });
+        setPropertyCnt((prevCount) => prevCount - 1);
+    };
+
+    const handleChange = (index, field, value) => {
+        setSpecifications((prevSpecs) => {
+            const newSpecs = [...prevSpecs];
+            newSpecs[index][field] = value;
+            return newSpecs;
+        });
+    };
 
     return (
         <div className="form__grp">
             <label htmlFor="Property">Specifications</label>
-            {Array.from({ length: propertyCnt }).map((_, i) => (
-                <div key={i} className="flex gap-2">
+
+            {Array.from({ length: propertyCnt || 1 }).map((_, i) => (
+                <div key={i} className="flex gap-2 items-center">
                     <input
+                        value={specifications?.[i]?.key || ""}
                         type="text"
                         name="key"
                         placeholder="Specification Name"
+                        onChange={(e) => handleChange(i, "key", e.target.value)}
                     />
                     <input
+                        value={specifications?.[i]?.value || ""}
                         type="text"
                         name="value"
                         placeholder="Specification Value"
+                        onChange={(e) =>
+                            handleChange(i, "value", e.target.value)
+                        }
                     />
+                    <span
+                        className="bg-red-600 px-2 py-1 text-white text-sm cursor-pointer font-semibold rounded hover:bg-red-500 mx-2 self-center"
+                        type="decrease"
+                        onClick={() => handleDelete(i)}
+                    >
+                        X
+                    </span>
                 </div>
             ))}
             <span
                 className="bg-blue-600 px-2 py-1 text-white text-sm cursor-pointer font-semibold rounded hover:bg-blue-500 mx-2 self-start"
                 type="increase"
-                onClick={() => setPropertyCnt(propertyCnt + 1)}
+                onClick={() => {
+                    if (specifications?.[propertyCnt - 1]?.key === "") {
+                        return;
+                    }
+
+                    setPropertyCnt(propertyCnt + 1);
+                    setSpecifications((prevSpecs) => {
+                        const newSpecs = [...prevSpecs];
+                        newSpecs.push({ key: "", value: "" });
+                        return newSpecs;
+                    });
+                }}
             >
                 Add Field +
             </span>
